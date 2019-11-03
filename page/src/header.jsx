d4ddfedb0aeeb543fa3c405e8c2d2bfcd92c27b1
ianssenne <ianssenne@gmail.com>
2019-11-03 02:31:29 -0800

    forgot to put something in the header

--- page/src/header.jsx
+++ page/src/header.jsx
@@ -2,6 +2,6 @@ import fw from "ians-fw";
 fw.css`.header-host{position:absolute;top:0px;left:0px;width:100vw;height:100vh;}`;
 export default function Header(props) {
     return <div className="header-host">
-
+        <h1>Header</h1>
     </div>
 }
